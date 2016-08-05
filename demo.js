4502012600695950aa105b50a1c219e9d552eead
Andy Principe <aprincipe@vsapartners.com>
2016-08-05 14:52:14 -0400

    adds thermin2.js - redid library without jQurey dependency

--- demo.js
+++ demo.js
@@ -1,19 +1,28 @@
 $( document ).ready(function() {
 
 	// THERMIN-iZE!
-	$('.thermin-demo').Thermin();
+	// $('.thermin-demo').Thermin();
+	Thermin({
+		'id':'thermin'
+	});
 
 	// handle change of form inputs
 	$('form#select-wrapper').change(function(e){
 		$('.thermin-demo').remove();
 		
-		$("#select-wrapper").parent().append("<div class='thermin-demo'></div>")
+		$("#select-wrapper").parent().append("<div class='thermin-demo' id='thermin'></div>")
 
 		var oscType = $("#osc-type option:selected").val();
 		var minFreq = $("#minFreqInputId").val();
 		var maxFreq = $("#maxFreqInputId").val();
 
-		$('.thermin-demo').Thermin({
+		// $('.thermin-demo').Thermin({
+		// 	"oscType": oscType,
+		// 	"minFreq": minFreq,
+		// 	"maxFreq": maxFreq
+		// });
+		Thermin({
+			"id": "thermin",
 			"oscType": oscType,
 			"minFreq": minFreq,
 			"maxFreq": maxFreq
